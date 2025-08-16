05b3c915edfa166e2b4b882c1fbc91fabf046797
harmanbansal10 <your-email@example.com>
2025-08-16 16:50:08 -0400

    first commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "solvia-better-everyday",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
